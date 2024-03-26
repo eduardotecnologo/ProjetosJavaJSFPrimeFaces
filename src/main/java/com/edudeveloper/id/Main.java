628d3d94690209a846cd2c36ecfe01fe3da84d74
eduardotecnologo <eduardotecnologo@hotmail.com.com>
2024-03-26 20:46:47 -0300

    Install Tomcat

--- src/main/java/com/edudeveloper/id/Main.java
+++ src/main/java/com/edudeveloper/id/Main.java
@@ -2,6 +2,9 @@ package com.edudeveloper.id;
 
 public class Main {
     public static void main(String[] args) {
+        Pedidos pedidos = new Pedidos();
         
+        RelatorioService relatorioService = new RelatorioService();
+        //relatorioService.setPedidos(pedidos);
     }
 }
--- src/main/java/com/edudeveloper/id/RelatorioService.java
+++ src/main/java/com/edudeveloper/id/RelatorioService.java
@@ -2,15 +2,26 @@ package com.edudeveloper.id;
 
 import java.math.BigDecimal;
 
+import javax.inject.Inject;
+
 public class RelatorioService {
     
+	@Inject
 	private Pedidos pedidos;
     
     public RelatorioService(Pedidos pedidos) {
     	this.pedidos = pedidos;
     }
     
+    public RelatorioService() {
+
+    }
+    
     public BigDecimal totalPedidosMesAtual(){
         return pedidos.totalPedidosMesAtual();
     }
+    
+    public void setPedidos(Pedidos pedidos) {
+		this.pedidos = pedidos;
+	}
 }
